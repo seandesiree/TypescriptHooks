af2f8c1f819b3689de969b641d3f2dc61b02c648
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2024-10-30 17:05:51 -0400

    upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter'; 
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Counter Application</h1>
+            <Counter /> 
+        </div>
+    );
+};
+
+export default App;
